f1fa4424d7f488949424649c5c3a0b86df334659
omkarshindecomp24 <omkar.shinde_comp24@pccoer.in>
2026-09-17 00:06:45 +0530

    Time: 311 ms (13.57%) | Memory: 61 MB (5.07%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,44 +1,43 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
         HashSet<List<Integer>> hs = new HashSet<>();
-
         List<List<Integer>> ls = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i]>0)break;
-            twosum(nums, i , -nums[i], hs);
+        for (int i = 0; i < nums.length && nums[i]<=0; i++) {
+            twosum(nums, i, -nums[i], ls,hs);
         }
-        for (List<Integer> x : hs) {
-            ls.add(x);
-        }
+        // for (List<Integer> x : hs) {
+        //     ls.add(x);
+        // }
         return ls;
 
     }
 
-    void twosum(int[] nums, int i, int target, HashSet<List<Integer>> hs) {
-      //  HashMap<Integer, Integer> hm = new HashMap<>();
-        int end= nums.length-1;
-        int st=i+1;
+    void twosum(int[] nums, int i, int target, List<List<Integer>> ls, HashSet<List<Integer>> hs) {
+        int end = nums.length - 1;
+        int st = i + 1;
 
-        while(st<end){
-            if(nums[st]+nums[end]==target){
-                        ArrayList<Integer> al=new ArrayList<>();
+        while (st < end) {
+            if (nums[st] + nums[end] == target) {
+                ArrayList<Integer> al = new ArrayList<>();
                 al.add(nums[i]);
                 al.add(nums[st]);
                 al.add(nums[end]);
-                hs.add(al);
-               st++;
-               end--;
-               continue;
+
+             if(!hs.contains(al)) {  ls.add(al);
+             hs.add(al);
+             }
+                st++;
+                end--;
+                continue;
             }
-            if(nums[st]+nums[end]>target){
-                       end--;
-            }else{
+            if (nums[st] + nums[end] > target) {
+                end--;
+            } else {
                 st++;
             }
         }
-        
+
         return;
     }
 }
