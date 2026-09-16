74699fb767be10272a4cd1bd33919b1bbe615bbb
omkarshindecomp24 <omkar.shinde_comp24@pccoer.in>
2026-09-17 03:43:29 +0530

    Time: 1 ms (95.72%) | Memory: 43.1 MB (63.53%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder str = new StringBuilder();
+        int k = Math.max(word1.length(), word2.length());
+        int i = 0;
+        while (i < k) {
+            if (i < word1.length())
+                str.append(word1.charAt(i));
+            if (i < word2.length())
+                str.append(word2.charAt(i));
+            i++;
+        }
+        return str.toString();
+    }
+}
