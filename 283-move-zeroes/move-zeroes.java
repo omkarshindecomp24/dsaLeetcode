f5bab8cfb6708727c8fdf36140029c3f01776d9b
omkarshindecomp24 <omkar.shinde_comp24@pccoer.in>
2026-09-18 00:08:16 +0530

    Time: 8 ms (7.05%) | Memory: 47.7 MB (76.07%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,14 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        List<Integer> ls=new ArrayList<>();
-        for(int x:nums){
-            if(x!=0){
+        List<Integer> ls = new ArrayList<>();
+        int l=0,j=0;
+        for (int x : nums) {
+            
+            if (x != 0) {
                 ls.add(x);
+                nums[j]=0;
+                nums[l++]=ls.get(ls.size()-1);
             }
+            j++;
         }
-        Arrays.fill(nums,0);
-        for(int i=0;i<ls.size();i++){
-            nums[i]=ls.get(i);
-        }
+        // Arrays.fill(nums, 0);
+        // for (int i = 0; i < ls.size(); i++) {
+        //     nums[i] = ls.get(i);
+        // }
     }
 }
