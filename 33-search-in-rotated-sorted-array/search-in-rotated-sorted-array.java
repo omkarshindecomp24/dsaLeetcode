13a51b09a21c1fa0df3e340315f02e707ae80264
omkarshindecomp24 <omkar.shinde_comp24@pccoer.in>
2026-09-19 04:01:20 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (28.30%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1, mid = 0, k = -1;
+        while (low <high) {
+            mid = low + (high - low) / 2;
+            if (nums[low] <=nums[mid] && nums[mid] <=nums[high]) {
+                k = low;
+                // if(low==mid&& mid==high)k=low+1;
+                break;
+            } else {
+                if (nums[low] <=nums[mid] && nums[mid] >=nums[high]) {
+                    low = mid+1 ;
+                } else if (nums[low] >=nums[mid] && nums[mid] <= nums[high]) {
+                    high = mid ;
+                }
+            }
+        }
+
+        // while (k != 0 && nums[k - 1] < nums[k]) {
+        //     k--;
+        // }
+        int t = k;
+        low =  k==-1?Math.max(low,high):k;
+        t=low;
+        k = -1;
+        high = nums.length - 1;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                k = mid;
+                break;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+        }
+        low = 0;
+        int p = -1;
+        high = t - 1;
+        //  high = nums.length - 1;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                p = mid;
+                break;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+        }
+        return p == -1 ? k : p;
+
+    }
+
+}
