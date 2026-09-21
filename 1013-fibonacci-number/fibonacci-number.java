28efb70b83c9f87a7f808b53bf727c8dde07498d
omkarshindecomp24 <omkar.shinde_comp24@pccoer.in>
2026-09-21 09:05:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (70.18%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,7 +1,18 @@
 class Solution {
     public int fib(int n) {
-        if(n==0)return 0;
-        if(n==1 || n==2)return 1;
-        return fib(n-1)+fib(n-2);
+        int a=0;
+        int b=1;
+        int c=1;
+       int k=2;
+       if(n==1 || n==2)return 1;
+       if(n==0)return 0;
+        while(k!=n){
+          a=b;
+          b=c;
+          c=a+b;
+          k++;
+
+        }
+        return c;
     }
 }
